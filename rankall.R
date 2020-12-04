4f024613d4513fe75541c844cbeb494ac6a8936b
tanka-mesh <atanyakamesh@gmail.com>
2020-12-05 03:46:28 +1100

    init commit of rankall function

---
+++ rankall.R
@@ -0,0 +1,31 @@
+rankall <- function(outcome, num = "best"){
+        df <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available", stringsAsFactors = FALSE)
+        p.outcomes <- c("heart attack"=11, "heart failure"=17,"pneumonia"=23) ## creates index for possible outcomes
+        if(!(state %in% df[,7])){
+                stop("invalid state")
+        }
+        if(!(outcome %in% names(p.outcomes))){
+                stop("invalid outcome")
+        }
+        my_data <- df[,c(2,7,p.outcomes[[outcome]])] ## extracting required subset of data - hospital name, 
+        ## state,and mortality rate for specified outcome
+        names(my_data) <- c("hospital","state","mortality") ## changing column names
+        good <- complete.cases(my_data)
+        my_data <- my_data[good,] ## removing NA values
+        my_data <- my_data[order(my_data$state, my_data$mortality, my_data$hospital),] ##sorting the data in 
+        ## ascending order of state, mortality, & hospital name
+        s_data <- split(my_data, my_data$state) ## splitting the data into data frames by state
+        s_names <- names(s_data)
+        h_names <- lapply(s_data,function(x, ...){
+                if(num == "best"){
+                        x[1,1]
+                } else if(num == "worst"){
+                        x[nrow(s_data),1]
+                } else {
+                        x[num,1]
+                }
+        })
+        h_vect <- unlist(h_names)
+        rank_df <- data.frame(hospital = h_vect, state = s_names, row.names = s_names)
+        rank_df
+}
